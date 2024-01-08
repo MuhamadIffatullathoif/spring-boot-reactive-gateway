d277fb56175fd57bcbfde149054b01dfbbe632e5
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-08 07:19:52 +0700

    01. Starter Project

---
+++ src/main/java/com/iffat/springboot/reactivegateway/SpringBootReactiveGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.iffat.springboot.reactivegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactiveGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReactiveGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iffat/springboot/reactivegateway/SpringBootReactiveGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iffat.springboot.reactivegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactiveGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
